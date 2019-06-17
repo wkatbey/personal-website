c0ebfbaea9edd5e236137c228fe5de721bbbb01c
Wassim Katbey <fv6124@wayne.edu>
2019-06-17 19:38:37 -0400

    Adding routing

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    path('', views.index, name='index')
+]
--- personal_website/urls.py
+++ personal_website/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+
 from django.urls import include, path
 
 urlpatterns = [
     path('', include('static_website.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
