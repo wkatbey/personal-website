aadd7cf50e17f7870335821141a93bafd0490566
Wassim Katbey <fv6124@wayne.edu>
2019-06-17 23:39:04 -0400

    Continued work on forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from django.forms import ModelForm
-from models import BlogEntry
+from blog.models import BlogEntry
 
 class BlogForm(ModelForm):
     class Meta:
         model = BlogEntry
-        fields = [
+        fields = (
             'title',
             #'primary_image',
             'text_entry'
-        ]
+        )
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views import View
+from blog.forms import BlogForm
 
 class IndexView(TemplateView):
     template_name = 'blog/index.html'
 
 class CreateBlogView(View):
      def get(self, request):
-        return render(request, 'blog/create-blog.html', {})
+        blog_creation_form = BlogForm()
+        context = {
+            'blog_creation_form': blog_creation_form
+        }
+        return render(request, 'blog/create-blog.html', context)
