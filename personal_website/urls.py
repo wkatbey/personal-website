ab24d04b38d572e9fe43fcb01203cfe2aa862102
wkatbey <lunarxparallax@gmail.com>
2021-11-26 23:01:48 -0500

    Removing blog from website. Plans for an external blogging site

--- personal_website/urls.py
+++ personal_website/urls.py
@@ -4,7 +4,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('static_website.urls')),
-    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
 ]
