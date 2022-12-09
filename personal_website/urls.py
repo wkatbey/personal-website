7a54c63993acba2c9a379239e8a064546a4628dd
Wassim Katbey <lunarxparallax@gmail.com>
2022-12-08 22:14:09 -0500

    checkpoint

--- personal_website/urls.py
+++ personal_website/urls.py
@@ -4,5 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('static_website.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('user/', include('user.urls'))
 ]
